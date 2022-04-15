3d76e9a92be692b1662591414645275bf0425479
Patrick Huang <huangpatrick16777216@gmail.com>
2022-04-15 11:01:50 -0700

    Client handler.

---
+++ server/Client.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * This class handles a client server side.
+ */
+public class Client {
+    private Socket conn;
+    private Reader in;
+    private Writer out;
+
+    public Client(Socket conn) {
+        this.conn = conn;
+        in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+    }
+}
