a9e98eab74cfcae6fffcdf497862ad6f76d6a304
PeaceOfPi <kagarwal.private@gmail.com>
2022-04-27 12:27:13 -0700

    creating game logic classes

--- server/ExplodingKittens.java
+++
@@ -1,9 +0,0 @@
-public class ExplodingKittens {
-
-    ArrayList<Player> pList;
-    public ExplodingKittens(Player p1, Player p2, Player p3, Player p4)
-    {
-        pList = new ArrayList<Player>(4);
-        
-    }
-}
