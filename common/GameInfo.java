4084a44bbf8a16a44abd5a063033bf9ec5785d47
PeaceOfPi <kagarwal.private@gmail.com>
2022-05-19 13:02:24 -0700

    stuff

---
+++ common/GameInfo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class GameInfo
+{
+    int deckCardCount;
+    int[] playerCardCount;
+    ArrayList<Integer>[] playerHands;
+    int activePlayerNumber;
+    String activePlayerId;
+    int topCardOfDeck;
+
+    public GameInfo(Game game)
+    {
+         
+    }
+
+}
