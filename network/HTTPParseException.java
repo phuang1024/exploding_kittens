884eb914a3cfa106419567d1c8f88ec8ddd19602
Patrick Huang <huangpatrick16777216@gmail.com>
2022-05-19 12:27:52 -0700

    Networking javadocs.

--- network/HTTPParseException.java
+++ network/HTTPParseException.java
@@ -2,6 +2,10 @@
  * Error while parsing HTTP.
  */
 public class HTTPParseException extends Exception {
+    /**
+     * Initialize with a message.
+     * @param msg  Message.
+     */
     public HTTPParseException(String msg) {
         super(msg);
     }
